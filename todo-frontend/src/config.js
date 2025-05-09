43f7d556fcfe1e4f203a06935aa660afb8fa0e9f
MichaelR13 <michaels.rojas13@csu.fullerton.edu>
2025-05-09 12:01:42 -0700

    Updated API handling in Lambda functions and redeployed SAM

--- todo-frontend/src/config.js
+++ todo-frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://ejd3rx28k7.execute-api.us-east-1.amazonaws.com/Prod';
+export const API_URL = 'https://083j691mkf.execute-api.us-east-1.amazonaws.com/Prod';
 export const API_KEY = 'jf7L4oMCZv5ZzcP7Elsco6ZZNu7hwmep8k8pWZqd';
